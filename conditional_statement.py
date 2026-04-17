dd0566639f9c2f7e460afee2420f98a529353d14
shiva <k.shivashankar.in@gmail.com>
2026-04-17 20:03:35 +0530

    Conditional statement ocmpleted

---
+++ conditional_statement.py
@@ -0,0 +1,80 @@
+# #  IF condition
+
+# age = 12
+
+# if age > 18:  # 12 > 18
+#     print("Pls do vote")
+# else :
+#     print("Pls wait for some more years to vote")
+    
+
+# """
+#     mark  (input)
+
+#     mark < 35 => fail
+#     mark < 50 => Pass
+#     mark < 75  => Good 
+#     mark > 75 => Excellant
+        
+# """
+
+
+# mark = int(input("Enter your mark: "))
+
+
+# status = "inactive"
+
+
+
+# if (mark < 0 or mark > 100) or status == "inactive":
+#     print("Invalid mark")
+# else:
+#     if mark < 35:
+#         print("Fail")
+#     elif mark < 50:
+#         print("Pass")
+#     elif mark < 75:
+#         print("Good")
+#     else:
+#         print("Excellent")
+
+
+# # age = 90
+
+# # if age < 0 or age > 120:
+# #     print("Invalid age")
+# # else:
+# #     if age > 18 and age < 70:
+# #         print("You are eligible to work")
+# #     else:
+# #         print("You are not eligible to work")
+
+
+# # status = "In active"
+
+# # if not (status != "active"):
+# #     print("User is active")
+    
+    
+# status = True
+
+
+# if status:
+#     print("User is active")
+
+
+
+# # number = 6
+
+# # if number > 5:
+# #     print("Number is greater than 5")
+
+
+# # name = "Guna"
+
+# # if name != "Guna":
+# #     print("Hello this is not Guna")
+
+if True:
+    number = 10
+    print("This is true")
