9925dcb01614dada6e66c258ca42b07248269d3a
shiva <k.shivashankar.in@gmail.com>
2026-04-20 19:40:50 +0530

    Loop is completed

---
+++ loops.py
@@ -0,0 +1,53 @@
+# print("Hello World")
+# print("Hello World")
+# print("Hello World")
+# print("Hello World")
+# print("Hello World")
+
+
+# for i in range(5):
+#     print(str(i) + " Hello World")
+    
+
+#  start from 0
+# for i in range(5, 31):
+#     print(str(i) + " Hello World")
+
+
+#  start from 0
+# for i in range(0, 31, 5):
+#     print(str(i) + " Hello World")
+
+
+
+n = 0
+# while n < 5:
+#     print(str(n) + " is less than 5")
+#     n = n + 1
+    
+# print("While is stopped")
+    
+    
+    
+# break statement
+
+# while True:
+#     user_prompt = input("Enter user msg: ")
+#     if user_prompt == "stop":
+#         print("need break here")
+#         break
+    
+# print("While loop is stopped")
+
+
+# for i in range(10):
+#     if i == 8:
+#         continue
+#     print(str(i) + " Hello World")
+    
+    
+for i in range(3):
+   for j in range(3):
+       print(i, j)
+
+    
