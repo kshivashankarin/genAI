cf80539555899dee863dac4f635dbd2f9e93c7a6
shiva <k.shivashankar.in@gmail.com>
2026-04-27 19:53:29 +0530

    List is completed

---
+++ list.py
@@ -0,0 +1,120 @@
+print("List In python")
+
+
+
+# student1 = "Guna"
+# student2 = "Siva"
+# student3 = "Ravi"
+# student4 = "Kumar"
+
+# Creating
+
+# students = ["Guna", "Siva", "Ravi", "Kumar", "Ajith", 10, " ", 3.14, True, "Preveen"]
+
+# # print(type(students))
+
+# print(students[0], students[1], students[2], students[3], students[4], students[5], students[6], students[7], students[8])
+
+# name = "Guna"
+
+# print(name[0])
+
+# Accessing List Elements
+
+
+# my_list  = ["one", "two", "three", "four", "five"]
+
+# print(my_list[0:4])
+
+# Modifying List Elements
+
+# my_list[1] = "Two Hundred"
+
+# print(my_list)
+
+
+
+# # basic list operations
+# my_list.append("Sixty")
+
+# my_list.append("Seven")
+# print(my_list)
+
+# my_list.insert(1, "One Point Five")
+# my_list.insert(4, "Fourty")
+# print(my_list)
+
+
+
+# my_list.pop(4)
+# print(my_list)
+
+
+# my_list.remove("five")
+# print(my_list)
+
+
+# my_list.clear()
+
+# print(my_list)
+
+
+students = ["Guna", "Siva", "Ravi", "Kumar", "Ajith", "Preveen", "Vijay", "Arun", "Karthik", "Suresh", "Ramesh", "Sathish", "Mani", "Vimal", "Ganesh", "Praveen", "Vijay", "Arun", "Karthik", "Suresh", "Ramesh", "Sathish", "Mani", "Vimal", "Ganesh"]
+
+# for student in students:
+#     print(student)
+
+
+# print(len(students))
+
+# for i in range(len(students)):
+#     if students[i] == "Gugan":
+#         print("Guna is present in the list at index", i)
+
+
+# if "Guru" in students:
+#     print("Guru is present in the list")
+# else:
+#     print("Guru is not present in the list")
+
+students_marks = [85, 92, 85, 78, 90, 88, 95, 80, 82, 91, 89]
+
+# students_marks.sort()
+# print(students_marks)
+# students_marks.reverse()
+# print(students_marks)
+# print(students_marks.count(85))
+# print(students_marks)
+# print(students.index("Ravi"))
+# stu_mark = students_marks.copy()
+# print(stu_mark)
+
+
+# matrix = [[1.1, [1.2, 1.3]], [2.1, 2.2]]
+
+# print(matrix[0][1][1])
+
+
+# first_number, second_number, thirs_number, fourth_number = [1, 2, 3, 44]
+# # b = [4, 5, 6]
+
+# # first_number = a[0]
+# # second_number = a[1]
+# # thirs_number = a[2]
+
+# print(first_number, second_number, thirs_number, fourth_number)
+
+
+# a, b, *c = [1, 2, 3, 4, 8, 9, 3]
+
+
+# print(a)
+# print(b)
+# print(c)
+
+
+words = ["apple", "banana", "zzzz", "kiwi", "toyu"]
+
+words.sort(key=len)
+
+print(words)
